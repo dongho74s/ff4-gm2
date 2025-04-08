02627493b32b3cd06a65df5a8edb263e51d1b08c
SangJo Kim <kandrea@nate.com>
2025-04-08 20:23:00 +0900

    fix CADILLAC_CT6_2019

--- opendbc_repo/opendbc/car/gm/carstate.py
+++ opendbc_repo/opendbc/car/gm/carstate.py
@@ -175,7 +175,7 @@ class CarState(CarStateBase):
       else:
         ret.cruiseState.speed = pt_cp.vl["ECMCruiseControl"]["CruiseSetSpeed"] * CV.KPH_TO_MS
 
-      if self.CP.carFingerprint not in SDGM_CAR:
+      if self.CP.carFingerprint not in CAR.CADILLAC_CT6_2019:
         ret.stockAeb = cam_cp.vl["AEBCmd"]["AEBCmdActive"] != 0
       # openpilot controls nonAdaptive when not pcmCruise
       if self.CP.pcmCruise and self.CP.carFingerprint not in CC_ONLY_CAR:
