2340ae49ba60c6f2ceb1caadbf3e858fd57cc1db
SangJo Kim <kandrea@nate.com>
2025-04-08 20:24:44 +0900

    fix

--- opendbc_repo/opendbc/car/gm/carstate.py
+++ opendbc_repo/opendbc/car/gm/carstate.py
@@ -249,7 +249,7 @@ class CarState(CarStateBase):
       elif CP.carFingerprint not in CC_ONLY_CAR:
         cam_messages.append(("ASCMActiveCruiseControlStatus", 25))
 
-      if CP.carFingerprint not in SDGM_CAR:
+      if CP.carFingerprint not in CADILLAC_CT6_2019:
         cam_messages += [
           ("AEBCmd", 10),
         ]
