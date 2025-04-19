c20e4102d8bc356e12651cb8d7a0a88dbbba4e39
SangJo Kim <kandrea@nate.com>
2025-04-19 11:11:05 +0900

    from opgm

--- opendbc_repo/opendbc/safety/safety/safety_chrysler.h
+++ opendbc_repo/opendbc/safety/safety/safety_chrysler.h
@@ -207,15 +207,15 @@ static safety_config chrysler_init(uint16_t param) {
   };
 
   static const CanMsg CHRYSLER_TX_MSGS[] = {
-    {CHRYSLER_ADDRS.CRUISE_BUTTONS, 0, 3, false},
-    {CHRYSLER_ADDRS.LKAS_COMMAND, 0, 6, true},
-    {CHRYSLER_ADDRS.DAS_6, 0, 8, false},
+    {CHRYSLER_ADDRS.CRUISE_BUTTONS, 0, 3, .check_relay = false},
+    {CHRYSLER_ADDRS.LKAS_COMMAND, 0, 6, .check_relay = true},
+    {CHRYSLER_ADDRS.DAS_6, 0, 8, .check_relay = true},
   };
 
   static const CanMsg CHRYSLER_RAM_DT_TX_MSGS[] = {
-    {CHRYSLER_RAM_DT_ADDRS.CRUISE_BUTTONS, 2, 3, false},
-    {CHRYSLER_RAM_DT_ADDRS.LKAS_COMMAND, 0, 8, true},
-    {CHRYSLER_RAM_DT_ADDRS.DAS_6, 0, 8, false},
+    {CHRYSLER_RAM_DT_ADDRS.CRUISE_BUTTONS, 2, 3, .check_relay = false},
+    {CHRYSLER_RAM_DT_ADDRS.LKAS_COMMAND, 0, 8, .check_relay = true},
+    {CHRYSLER_RAM_DT_ADDRS.DAS_6, 0, 8, .check_relay = true},
   };
 
 #ifdef ALLOW_DEBUG
@@ -240,9 +240,9 @@ static safety_config chrysler_init(uint16_t param) {
   };
 
   static const CanMsg CHRYSLER_RAM_HD_TX_MSGS[] = {
-    {CHRYSLER_RAM_HD_ADDRS.CRUISE_BUTTONS, 2, 3, false},
-    {CHRYSLER_RAM_HD_ADDRS.LKAS_COMMAND, 0, 8, true},
-    {CHRYSLER_RAM_HD_ADDRS.DAS_6, 0, 8, false},
+    {CHRYSLER_RAM_HD_ADDRS.CRUISE_BUTTONS, 2, 3, .check_relay = false},
+    {CHRYSLER_RAM_HD_ADDRS.LKAS_COMMAND, 0, 8, .check_relay = true},
+    {CHRYSLER_RAM_HD_ADDRS.DAS_6, 0, 8, .check_relay = true},
   };
 
   const uint32_t CHRYSLER_PARAM_RAM_HD = 2U;  // set for Ram HD platform
@@ -275,7 +275,6 @@ const safety_hooks chrysler_hooks = {
   .init = chrysler_init,
   .rx = chrysler_rx_hook,
   .tx = chrysler_tx_hook,
-  .fwd = chrysler_fwd_hook,
   .get_counter = chrysler_get_counter,
   .get_checksum = chrysler_get_checksum,
   .compute_checksum = chrysler_compute_checksum,
--- opendbc_repo/opendbc/safety/safety/safety_nissan.h
+++ opendbc_repo/opendbc/safety/safety/safety_nissan.h
@@ -102,12 +102,12 @@ static bool nissan_tx_hook(const CANPacket_t *to_send) {
 
 static safety_config nissan_init(uint16_t param) {
   static const CanMsg NISSAN_TX_MSGS[] = {
-    {0x169, 0, 8, true},   // LKAS
-    {0x2b1, 0, 8, false},  // PROPILOT_HUD
-    {0x4cc, 0, 8, false},  // PROPILOT_HUD_INFO_MSG
-    {0x20b, 2, 6, false},  // CRUISE_THROTTLE (X-Trail)
-    {0x20b, 1, 6, false},  // CRUISE_THROTTLE (Altima)
-    {0x280, 2, 8, false}   // CANCEL_MSG (Leaf)
+    {0x169, 0, 8, .check_relay = true},   // LKAS
+    {0x2b1, 0, 8, .check_relay = true},   // PROPILOT_HUD
+    {0x4cc, 0, 8, .check_relay = true},   // PROPILOT_HUD_INFO_MSG
+    {0x20b, 2, 6, .check_relay = false},  // CRUISE_THROTTLE (X-Trail)
+    {0x20b, 1, 6, .check_relay = false},  // CRUISE_THROTTLE (Altima)
+    {0x280, 2, 8, .check_relay = true}    // CANCEL_MSG (Leaf)
   };
 
   // Signals duplicated below due to the fact that these messages can come in on either CAN bus, depending on car model.
