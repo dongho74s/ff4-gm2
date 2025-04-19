ea266bd6f3bd7f7a67a9bf34aa3c7c9956bf9fe6
SangJo Kim <kandrea@nate.com>
2025-04-19 11:18:16 +0900

    from opgm

--- opendbc_repo/opendbc/safety/safety.h
+++ opendbc_repo/opendbc/safety/safety.h
@@ -649,13 +649,20 @@ bool steer_torque_cmd_checks(int desired_torque, int steer_req, const TorqueStee
   if (controls_allowed) acc_main_on = controls_allowed;
   
   if (controls_allowed || aol_allowed) {
+    // Some safety models support variable torque limit based on vehicle speed
+    int max_torque = limits.max_torque;
+    if (limits.dynamic_max_torque) {
+      const float fudged_speed = (vehicle_speed.min / VEHICLE_SPEED_FACTOR) - 1.;
+      max_torque = interpolate(limits.max_torque_lookup, fudged_speed) + 1;
+      max_torque = CLAMP(max_torque, -limits.max_torque, limits.max_torque);
+    }
     // *** global torque limit check ***
-    violation |= max_limit_check(desired_torque, limits.max_steer, -limits.max_steer);
+    violation |= max_limit_check(desired_torque, max_torque, -max_torque);
 
     // *** torque rate limit check ***
     if (limits.type == TorqueDriverLimited) {
       violation |= driver_limit_check(desired_torque, desired_torque_last, &torque_driver,
-                                      limits.max_steer, limits.max_rate_up, limits.max_rate_down,
+                                      max_torque, limits.max_rate_up, limits.max_rate_down,
                                       limits.driver_torque_allowance, limits.driver_torque_multiplier);
     } else {
       violation |= dist_to_meas_check(desired_torque, desired_torque_last, &torque_meas,
--- opendbc_repo/opendbc/safety/safety_declarations.h
+++ opendbc_repo/opendbc/safety/safety_declarations.h
@@ -215,7 +215,6 @@ typedef struct {
 
 bool safety_rx_hook(const CANPacket_t *to_push);
 bool safety_tx_hook(CANPacket_t *to_send);
-uint32_t get_ts_elapsed(uint32_t ts, uint32_t ts_last);
 int to_signed(int d, int bits);
 void update_sample(struct sample_t *sample, int sample_new);
 bool get_longitudinal_allowed(void);
