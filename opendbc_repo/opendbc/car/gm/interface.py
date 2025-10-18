61bd9bbceea0ac8c50b935a375148a40f174e95e
dongho74s <165978235+dongho74s@users.noreply.github.com>
2025-10-18 22:28:04 +0900

    Add support for new Chevrolet Trailblazer model

--- opendbc_repo/opendbc/car/gm/interface.py
+++ opendbc_repo/opendbc/car/gm/interface.py
@@ -260,8 +260,25 @@ class CarInterface(CarInterfaceBase):
       ret.startingState = True
       ret.startAccel = 1.9
 
-    elif candidate in (CAR.CHEVROLET_TRAILBLAZER, CAR.CHEVROLET_TRAILBLAZER_CC):
+    elif candidate in (CAR.CHEVROLET_TRAILBLAZER, CAR.CHEVROLET_NEW_TRAILBLAZER, CAR.CHEVROLET_TRAILBLAZER_CC)::
+      ret.networkLocation = NetworkLocation.fwdCamera
+      ret.openpilotLongitudinalControl = True
+      ret.pcmCruise = False
+      ret.alphaLongitudinalAvailable = True
       ret.steerActuatorDelay = 0.2
+      ret.minEnableSpeed = -1 * CV.MPH_TO_MS
+      ret.minSteerSpeed = 7 * CV.MPH_TO_MS
+      ret.longitudinalTuning.kpBP = [0.]
+      ret.longitudinalTuning.kpV = [1.0]
+      ret.longitudinalTuning.kiBP = [0.]
+      ret.longitudinalTuning.kiV = [.3]
+      ret.longitudinalTuning.kf = 1.0
+      ret.stoppingDecelRate = 0.8 # brake_travel/s while trying to stop
+      ret.vEgoStopping = 0.4
+      ret.vEgoStarting = 0.35
+      ret.stopAccel = -0.5
+      ret.startingState = True
+      ret.startAccel = 1.0
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     elif candidate in (CAR.CHEVROLET_SUBURBAN, CAR.CHEVROLET_SUBURBAN_CC):
